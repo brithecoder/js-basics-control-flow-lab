6d5789e8c58c20e598c8b5b470840d808d321021
Brianna Gordon <brigordon574@gmail.com>
2021-05-19 09:26:45 -0700

    commit tree df8e17b02b667574ee02a4f05f0fa98bd5b176b3

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result 
+  if (ride <= 400 ){
+    return 'This one is on me!';
+  }
+    if (ride > 2000 && ride < 2500){
+      return 'I will gladly take your thirty bucks.';
+  }
+     else (ride <=2500)
+       return 'No can do.';
+   return result 
+  }
+
+
+function ternaryCheckCity(city){
+   let result 
+   if (city === 'NYC'){
+   return `Ok, sounds good.`;
+   }
+   if (city !==`NYC`){
+     return `No go.`;
+   }
+     return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case `generous`:
+      return `Thank you so much.`;
+    case `not as generous`:
+       return `Thank you.`;
+      default:
+       return `Bye.`;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
 }
